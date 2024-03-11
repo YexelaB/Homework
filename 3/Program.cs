faed2f8f7db8325c30c12411f829fa2f4901ee28
amnota <amnota@rambler.ru>
2024-03-11 20:44:18 +0300

    Треться задача

---
+++ 3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте произвольный массив. Выведете 
+// его элементы, начиная с конца. Использовать 
+// рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+void PrintArrayReversed(int[] array, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+
+    Console.Write($"{array[index]} ");
+    PrintArrayReversed(array, index - 1);
+}
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+PrintArrayReversed(array, array.Length - 1);
